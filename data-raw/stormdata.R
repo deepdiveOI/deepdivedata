a4031a8213a0a710a8d7764ad7da0132166a322c
Emily Allen <emily.allen@coveocean.com>
2022-07-29 08:40:57 -0300

    committed from R - changed stormData to weatherData

--- data-raw/stormdata.R
+++ data-raw/stormdata.R
@@ -1,3 +1,3 @@
 
-stormData <- read_csv("C:/Users/EAllen/OneDrive - COVE/Deep Dive Into Data/learnr/StormData.csv")
-use_data(stormData, overwrite = TRUE)
+weatherData <- read_csv("C:/Users/EAllen/OneDrive - COVE/Deep Dive Into Data/learnr/WeatherData.csv")
+use_data(weatherData, overwrite = TRUE)
