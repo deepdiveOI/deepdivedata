86dec9cdf42cf400a566ebe4aabe5579b8b315b3
Emily Allen <emily.allen@coveocean.com>
2022-09-20 16:01:43 -0300

    commit from RStudio

--- data-raw/harbourdata.R
+++ data-raw/harbourdata.R
@@ -1,3 +1,2 @@
-
 harbourData <- read_csv("C:/Users/EAllen/OneDrive - COVE/Deep Dive Into Data/learnr/TempAndTideData.csv")
-use_data(tempandtideData, overwrite = TRUE)
+use_data(harbourData, overwrite = TRUE)
